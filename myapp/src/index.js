ef6de165e0a63ba9ad902b2e34eca42286c806b7
Heggy Castaneda <heggy_castaneda@gap.com>
2019-07-03 16:18:09 -0700

    feat: add only hook system

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -31,43 +31,4 @@ const App = () => {
   return <div className="border red">{content}</div>
 };
 
-class App extends React.Component {
-  state = { 
-    lat: null, 
-    errorMessage: '' 
-  };
-
-// componentDidMount() only gets called once
-  componentDidMount() {
-    // console.log('My component was rendered to the screen.');
-    
-  }
-
-  componentDidUpdate() {
-    console.log('My component was just updated - it re-rendered!');
-  }
-
-  renderContent() {
-    if (this.state.errorMessage && !this.state.lat) {
-      return <div>Error: {this.state.errorMessage}</div>;
-    }
-
-    if (!this.state.errorMessage && this.state.lat) {
-      return <SeasonDisplay lat={this.state.lat} />
-    }
-
-    return <Spinner message="Please accept location request"/>;
-  }
-
-
-  // React requires we have to define render method!! return JSX
-  render() {
-    return (
-      <div className="border red">
-        {this.renderContent()}
-      </div>
-    )
-  }
-}
-
 ReactDOM.render(<App />, document.querySelector('#root'));
